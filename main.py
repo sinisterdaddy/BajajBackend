fde2e099742e30e5a4a36d6d0a17beee3660c8e4
Radha Krishna <krishna.garg010604@gmail.com>
2024-08-25 11:37:17 +0530

    upd

--- main.py
+++ main.py
@@ -36,4 +36,5 @@ def get_operation_code():
     return jsonify({"operation_code": 1}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
